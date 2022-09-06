17165075e049303a348e69711118e849d155a8cc
suhyeon chae <ddd8177@gmail.com>
2022-09-06 23:03:33 +0900

    쿠키 사용해서 이메일 기억하기 기능 완료

--- sp5-chap11/src/main/java/controller/LoginController.java
+++ sp5-chap11/src/main/java/controller/LoginController.java
@@ -2,6 +2,7 @@ package controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,8 @@ import spring.AuthInfo;
 import spring.AuthService;
 import spring.WrongIdPasswordException;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 @Controller
@@ -21,12 +24,18 @@ public class LoginController {
     }
 
     @GetMapping
-    public String form(LoginCommand loginCommand){
+    public String form(LoginCommand loginCommand , @CookieValue(value = "REMEMBER", required = false) Cookie rCookie){
+        // 이름이 "REMEMBER"인 쿠키를 Cookie 타입으로 전달받는다. 지정한 이름을 가진 쿠기가 존재하지 않을 수도 있다면 required 속성값을 false로 지정
+
+        if(rCookie != null){
+            loginCommand.setEmail(rCookie.getValue());
+            loginCommand.setRememberEmail(true);
+        }
         return "login/loginForm";
     }
 
     @PostMapping
-    public String submit(LoginCommand loginCommand , Errors errors , HttpSession session){
+    public String submit(LoginCommand loginCommand , Errors errors , HttpSession session, HttpServletResponse response){
         new LoginCommandValidator().validate(loginCommand, errors);
         if(errors.hasErrors()){
             return "login/loginForm";
@@ -37,6 +46,15 @@ public class LoginController {
                     loginCommand.getEmail(), loginCommand.getPassword()
             );
             session.setAttribute("authInfo", authInfo);
+
+            Cookie rememberCookie = new Cookie("REMEMBER", loginCommand.getEmail());
+            rememberCookie.setPath("/");
+            if (loginCommand.isRememberEmail()){
+                rememberCookie.setMaxAge(60*60*24*30); // 30일동안 유지
+            } else {
+                rememberCookie.setMaxAge(0);
+            }
+            response.addCookie(rememberCookie);
 
             //TODO세션에 authInfo 저장해야함
             return "login/loginSuccess";
